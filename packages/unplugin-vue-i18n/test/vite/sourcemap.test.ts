2f3db5a124c1c365ee0bf55c3904fa0ad5fc0ee7
kazuya kawaguchi <kawakazu80@gmail.com>
2022-05-07 00:55:26 +0900

    refactor

--- packages/unplugin-vue-i18n/test/vite/sourcemap.test.ts
+++ packages/unplugin-vue-i18n/test/vite/sourcemap.test.ts
@@ -1,55 +1,57 @@
 import { resolve } from 'pathe'
 import { bundleVite, bundleAndRun } from '../utils'
 
-describe('resource files', () => {
-  const options = {
-    sourcemap: true,
-    target: './fixtures/locales/',
-    include: [resolve(__dirname, '../fixtures/locales/**')]
-  }
+/**
+ * TODO:
+ *  custom blocks source map cannot confirm fully supporting
+ *  because, seem that vite does not support source map for custom blocks...
+ */
 
-  test('json', async () => {
-    const { map } = await bundleAndRun('ja.json', bundleVite, options)
-    expect(map.mappings).toMatchSnapshot()
-  })
+const options = {
+  sourcemap: true,
+  target: './fixtures/locales/',
+  include: [resolve(__dirname, '../fixtures/locales/**')]
+}
 
-  test('json5', async () => {
-    const { map } = await bundleAndRun('en.json5', bundleVite, options)
-    expect(map.mappings).toMatchSnapshot()
-  })
-
-  test('yaml', async () => {
-    const { map } = await bundleAndRun('ko.yaml', bundleVite, options)
-    expect(map.mappings).toMatchSnapshot()
-  })
+test('resource files: json', async () => {
+  const { map } = await bundleAndRun('ja.json', bundleVite, options)
+  expect(map.mappings).toMatchSnapshot()
 })
 
-describe('custom blocks', () => {
-  test('json', async () => {
-    const { map } = await bundleAndRun('basic.vue', bundleVite, {
-      sourcemap: true
-    })
-    expect(map.mappings).toMatchSnapshot()
-  })
+test('resource files: json5', async () => {
+  const { map } = await bundleAndRun('en.json5', bundleVite, options)
+  expect(map.mappings).toMatchSnapshot()
+})
 
-  test('yaml', async () => {
-    const { map } = await bundleAndRun('yaml.vue', bundleVite, {
-      sourcemap: true
-    })
-    expect(map.mappings).toMatchSnapshot()
-  })
+test('resource files: yaml', async () => {
+  const { map } = await bundleAndRun('ko.yaml', bundleVite, options)
+  expect(map.mappings).toMatchSnapshot()
+})
 
-  test('yml', async () => {
-    const { map } = await bundleAndRun('yml.vue', bundleVite, {
-      sourcemap: true
-    })
-    expect(map.mappings).toMatchSnapshot()
+test('custom blocks: json', async () => {
+  const { map } = await bundleAndRun('basic.vue', bundleVite, {
+    sourcemap: true
   })
+  expect(map.mappings).toMatchSnapshot()
+})
 
-  test('json5', async () => {
-    const { map } = await bundleAndRun('json5.vue', bundleVite, {
-      sourcemap: true
-    })
-    expect(map.mappings).toMatchSnapshot()
+test('custom blocks: yaml', async () => {
+  const { map } = await bundleAndRun('yaml.vue', bundleVite, {
+    sourcemap: true
   })
+  expect(map.mappings).toMatchSnapshot()
+})
+
+test('custom blocks: yml', async () => {
+  const { map } = await bundleAndRun('yml.vue', bundleVite, {
+    sourcemap: true
+  })
+  expect(map.mappings).toMatchSnapshot()
+})
+
+test('custom blocks: json5', async () => {
+  const { map } = await bundleAndRun('json5.vue', bundleVite, {
+    sourcemap: true
+  })
+  expect(map.mappings).toMatchSnapshot()
 })
--- packages/unplugin-vue-i18n/test/webpack/sourcemap.test.ts
+++ packages/unplugin-vue-i18n/test/webpack/sourcemap.test.ts
@@ -1,55 +1,51 @@
 import { resolve } from 'pathe'
 import { bundleWebpack, bundleAndRun } from '../utils'
 
-describe('resource files', () => {
-  const options = {
-    sourcemap: true,
-    target: './fixtures/locales/',
-    include: [resolve(__dirname, '../fixtures/locales/**')]
-  }
+const options = {
+  sourcemap: true,
+  target: './fixtures/locales/',
+  include: [resolve(__dirname, '../fixtures/locales/**')]
+}
 
-  test('json', async () => {
-    const { map } = await bundleAndRun('ja.json', bundleWebpack, options)
-    expect(map.mappings).toMatchSnapshot()
-  })
-
-  test('json5', async () => {
-    const { map } = await bundleAndRun('en.json5', bundleWebpack, options)
-    expect(map.mappings).toMatchSnapshot()
-  })
-
-  test('yaml', async () => {
-    const { map } = await bundleAndRun('ko.yaml', bundleWebpack, options)
-    expect(map.mappings).toMatchSnapshot()
-  })
+test('resource files: json', async () => {
+  const { map } = await bundleAndRun('ja.json', bundleWebpack, options)
+  expect(map.mappings).toMatchSnapshot()
 })
 
-describe('custom blocks', () => {
-  test.skip('json', async () => {
-    const { map } = await bundleAndRun('basic.vue', bundleWebpack, {
-      sourcemap: true
-    })
-    expect(map.mappings).toMatchSnapshot()
-  })
+test('resource files: json5', async () => {
+  const { map } = await bundleAndRun('en.json5', bundleWebpack, options)
+  expect(map.mappings).toMatchSnapshot()
+})
 
-  test.skip('yaml', async () => {
-    const { map } = await bundleAndRun('yaml.vue', bundleWebpack, {
-      sourcemap: true
-    })
-    expect(map.mappings).toMatchSnapshot()
-  })
+test('resource files: yaml', async () => {
+  const { map } = await bundleAndRun('ko.yaml', bundleWebpack, options)
+  expect(map.mappings).toMatchSnapshot()
+})
 
-  test.skip('yml', async () => {
-    const { map } = await bundleAndRun('yml.vue', bundleWebpack, {
-      sourcemap: true
-    })
-    expect(map.mappings).toMatchSnapshot()
+test('custom blocks: json', async () => {
+  const { map } = await bundleAndRun('basic.vue', bundleWebpack, {
+    sourcemap: true
   })
+  expect(map.mappings).toMatchSnapshot()
+})
 
-  test.skip('json5', async () => {
-    const { map } = await bundleAndRun('json5.vue', bundleWebpack, {
-      sourcemap: true
-    })
-    expect(map.mappings).toMatchSnapshot()
+test('custom blocks: yaml', async () => {
+  const { map } = await bundleAndRun('yaml.vue', bundleWebpack, {
+    sourcemap: true
   })
+  expect(map.mappings).toMatchSnapshot()
+})
+
+test('custom blocks: yml', async () => {
+  const { map } = await bundleAndRun('yml.vue', bundleWebpack, {
+    sourcemap: true
+  })
+  expect(map.mappings).toMatchSnapshot()
+})
+
+test('custom blocks: json5', async () => {
+  const { map } = await bundleAndRun('json5.vue', bundleWebpack, {
+    sourcemap: true
+  })
+  expect(map.mappings).toMatchSnapshot()
 })

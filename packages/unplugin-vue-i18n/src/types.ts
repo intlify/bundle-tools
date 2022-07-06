35d4cf50168e6fb806206140a6b8b0bf3ef354e8
kazuya kawaguchi <kawakazu80@gmail.com>
2022-07-06 21:03:20 +0900

    feat(unplugin-vue-i18n): support runtimeOnly option (#134)

--- packages/unplugin-vue-i18n/src/types.ts
+++ packages/unplugin-vue-i18n/src/types.ts
@@ -1,6 +1,7 @@
 export type SFCLangFormat = 'json' | 'json5' | 'yml' | 'yaml'
 export interface PluginOptions {
   include?: string | string[]
+  runtimeOnly?: boolean
   forceStringify?: boolean
   defaultSFCLang?: SFCLangFormat
   globalSFCScope?: boolean

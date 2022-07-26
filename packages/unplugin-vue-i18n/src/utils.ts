aef68a81adc3be0c773d599ddb7ddf12343f08a5
kazuya kawaguchi <kawakazu80@gmail.com>
2022-07-26 23:56:47 +0900

    fix(unplugin-vue-i18n): support unplugin v0.8 (#154)
    * fix(unplugin-vue-i18n): support unplugin v0.8
    
    * drop node v12

--- packages/unplugin-vue-i18n/src/utils.ts
+++ packages/unplugin-vue-i18n/src/utils.ts
@@ -1,11 +1,12 @@
 import { promises as fs } from 'fs'
+import pc from 'picocolors'
 
 export function warn(...args: unknown[]) {
-  console.warn(`[unplugin-vue-i18n] `, ...args)
+  console.warn(pc.yellow(pc.bold(`[unplugin-vue-i18n] `)), ...args)
 }
 
 export function error(...args: unknown[]) {
-  console.error(`[unplugin-vue-i18n] `, ...args)
+  console.error(pc.red(pc.bold(`[unplugin-vue-i18n] `)), ...args)
 }
 
 export async function getRaw(path: string): Promise<string> {

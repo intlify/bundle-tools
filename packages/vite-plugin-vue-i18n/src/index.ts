f82ce2c846d45a3edcfc7a2dfdd8e7516c74d3b1
Marek Vospel <73162071+marekvospel@users.noreply.github.com>
2022-08-10 19:08:45 +0200

    fix(vite-plugin-vue-i18n): use correct id in hmr (#160)
    * fix(vite-plugin-vue-i18n): use correct id in hmr
    
    * fix(vite-plugin-vue-i18n): use asVirtualId instead of getVirtualId, lin

--- packages/vite-plugin-vue-i18n/src/index.ts
+++ packages/vite-plugin-vue-i18n/src/index.ts
@@ -210,7 +210,7 @@ function pluginI18n(
     async handleHotUpdate({ file, server }) {
       if (/\.(json5?|ya?ml)$/.test(file)) {
         const module = server.moduleGraph.getModuleById(
-          INTLIFY_BUNDLE_IMPORT_ID
+          asVirtualId(INTLIFY_BUNDLE_IMPORT_ID)
         )
         if (module) {
           server.moduleGraph.invalidateModule(module)

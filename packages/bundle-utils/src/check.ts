e708ca5ce5f9a473a8cb24b2d4ab88edfe260dc4
kazuya kawaguchi <kawakazu80@gmail.com>
2022-09-27 12:30:11 +0900

    feat: add vue-i18n-bridge install checking (#185)
    * feat: add vue-i18n-bridge install checking
    
    * update snapshot

--- packages/bundle-utils/src/check.ts
+++ packages/bundle-utils/src/check.ts
@@ -31,3 +31,15 @@ export function checkInstallPackage(
     `${pkg} requires 'vue-i18n' or 'petite-vue-i18n' to be present in the dependency tree.`
   )
 }
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function checkVueI18nBridgeInstallPackage(debug: Function): boolean {
+  let ret = false
+  try {
+    debug(`vue-i18n-bridge load path: ${require.resolve('vue-i18n-bridge')}`)
+    ret = true
+  } catch (e) {
+    debug(`cannot find 'vue-i18n-bridge'`, e)
+  }
+  return ret
+}

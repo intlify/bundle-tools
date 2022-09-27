5a27d2efb58cb84805cbbc5fa9d3134a6fc3863b
kazuya kawaguchi <kawakazu80@gmail.com>
2022-09-27 12:35:47 +0900

    fix(bundle-utils): export `checkVueI18nBridgeInstallPackage`

--- packages/bundle-utils/src/index.ts
+++ packages/bundle-utils/src/index.ts
@@ -1,4 +1,8 @@
 export { CodeGenOptions, CodeGenResult, DevEnv } from './codegen'
 export { generate as generateJSON } from './json'
 export { generate as generateYAML } from './yaml'
-export { checkInstallPackage, InstalledPackage } from './check'
+export {
+  checkInstallPackage,
+  checkVueI18nBridgeInstallPackage,
+  InstalledPackage
+} from './check'

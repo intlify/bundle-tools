45eb36f89a5591b6da01dcdcb7bdfa450ea5b60f
kazuya kawaguchi <kawakazu80@gmail.com>
2022-09-27 12:10:02 +0900

    feat: add vue-i18n-bridge install checking

--- packages/bundle-utils/test/check.test.ts
+++ packages/bundle-utils/test/check.test.ts
@@ -1,5 +1,12 @@
-import { checkInstallPackage } from '../src/check'
+import {
+  checkInstallPackage,
+  checkVueI18nBridgeInstallPackage
+} from '../src/check'
 
 test('vue-i18n', () => {
   expect(checkInstallPackage('vue-i18n', jest.fn())).toBe('vue-i18n')
+})
+
+test('vue-i18n-bridge', () => {
+  expect(checkVueI18nBridgeInstallPackage(jest.fn())).toBe(false)
 })

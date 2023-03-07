ea1a0c63214bab868e4b105e15ee060cff372e65
kazuya kawaguchi <kawakazu80@gmail.com>
2023-03-07 23:59:45 +0900

    fix(bundle-utils): export js generate API (#231)
    * fix(bundle-utils): export js generate API
    
    * fix: update lock

--- packages/bundle-utils/src/index.ts
+++ packages/bundle-utils/src/index.ts
@@ -1,6 +1,7 @@
 export { CodeGenOptions, CodeGenResult, DevEnv } from './codegen'
 export { generate as generateJSON } from './json'
 export { generate as generateYAML } from './yaml'
+export { generate as generateJavaScript } from './js'
 export {
   getVueI18nVersion,
   checkInstallPackage,

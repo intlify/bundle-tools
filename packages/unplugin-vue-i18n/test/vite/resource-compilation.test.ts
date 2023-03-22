319615e29704c358fbf35afe4fed56800eeb5163
kazuya kawaguchi <kawakazu80@gmail.com>
2023-03-23 02:05:25 +0900

    feat(unplugin-vue-i18n): support strict message and escape html

--- packages/unplugin-vue-i18n/test/vite/resource-compilation.test.ts
+++ packages/unplugin-vue-i18n/test/vite/resource-compilation.test.ts
@@ -1,7 +1,18 @@
 import { resolve } from 'pathe'
 import { bundleVite, bundleAndRun } from '../utils'
-import { isFunction } from '@intlify/shared'
+import { isFunction, assign } from '@intlify/shared'
 import { createMessageContext } from '@intlify/core-base'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let spyConsoleError: any
+beforeEach(() => {
+  spyConsoleError = jest.spyOn(global.console, 'error').mockImplementation()
+})
+
+afterEach(() => {
+  spyConsoleError.mockReset()
+  spyConsoleError.mockRestore()
+})
 
 const options = {
   target: './fixtures/locales/',
@@ -56,4 +67,31 @@ test('dynamical resource with js / ts', async () => {
     ...options
   })
   expect(isFunction(module)).toBe(true)
+})
+
+test('strict message', async () => {
+  let occured = false
+  try {
+    await bundleAndRun('html.json', bundleVite, {
+      ...options
+    })
+  } catch (e) {
+    occured = true
+  }
+  expect(occured).toBe(true)
+})
+
+test('escape message', async () => {
+  const { module } = await bundleAndRun(
+    'html.json',
+    bundleVite,
+    assign(options, {
+      strictMessage: false,
+      escapeHtml: true
+    })
+  )
+  expect(module.hi(createMessageContext())).toBe(`&lt;p&gt;hi there!&lt;/p&gt;`)
+  expect(module.alert(createMessageContext())).toBe(
+    `&lt;script&gt;window.alert(&apos;hi there!&apos;)&lt;/script&gt;`
+  )
 })

7de6254cb552e39fa35e4b1e1abe6c9bfeefd890
kazuya kawaguchi <kawakazu80@gmail.com>
2023-03-29 18:28:24 +0900

    feat(bundle-utils): support esm/cjs dual package (#255)
    * feat(bundle-utils): support esm/cjs dual package
    
    * chore: add no hoist config
    
    * update

--- packages/bundle-utils/test/deps.test.ts
+++ packages/bundle-utils/test/deps.test.ts
@@ -1,3 +1,4 @@
+import { vi } from 'vitest'
 import {
   checkInstallPackage,
   checkVueI18nBridgeInstallPackage,
@@ -6,17 +7,17 @@ import {
 } from '../src/deps'
 
 test('vue-i18n', () => {
-  expect(checkInstallPackage('vue-i18n', jest.fn())).toBe('vue-i18n')
+  expect(checkInstallPackage('vue-i18n', vi.fn())).toBe('vue-i18n')
 })
 
 test('vue-i18n-bridge', () => {
-  expect(checkVueI18nBridgeInstallPackage(jest.fn())).toBe(false)
+  expect(checkVueI18nBridgeInstallPackage(vi.fn())).toBe(false)
 })
 
 test('loadModule', () => {
-  expect(loadModule('yaml-eslint-parser', jest.fn())).not.toBe(null)
+  expect(loadModule('yaml-eslint-parser', vi.fn())).not.toBe(null)
 })
 
 test('getVueI18nVersion', () => {
-  expect(getVueI18nVersion(jest.fn())).toBe('9')
+  expect(getVueI18nVersion(vi.fn())).toBe('9')
 })

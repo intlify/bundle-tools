26a13f6bc030a7746804057e1d28f34ed9100c65
Danielle Huisman <danielle@innico.nl>
2023-10-26 11:18:37 +0200

    Replace lodash.merge with a plain JS implementation

--- packages/unplugin-vue-i18n/src/index.ts
+++ packages/unplugin-vue-i18n/src/index.ts
@@ -703,11 +703,29 @@ async function generateBundleResources(
     }
   }
 
-  return `import merge from 'lodash.merge'
+  return `const isObject = (item) => item && typeof item === 'object' && !Array.isArray(item);
 
-export default merge({},
+const mergeDeep = (target, ...sources) => {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
+
+export default mergeDeep({},
   ${codes.map(code => `{${code}}`).join(',\n')}
-)`
+);`
 }
 
 async function getCode(

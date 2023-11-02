8d042fc1cdba041a512a33a7c5897fba65864f7b
Daniëlle Huisman <danielle@huisman.me>
2023-11-02 03:41:48 +0100

    feat: merging of locale files with the same name for `@intlify/unplugin-vue-i18n/messages` (#274)
    * Fix merging of locale files with the same name
    
    * Replace lodash.merge with a plain JS implementation
    
    * Update README.md

--- packages/unplugin-vue-i18n/src/index.ts
+++ packages/unplugin-vue-i18n/src/index.ts
@@ -782,9 +782,29 @@ async function generateBundleResources(
     }
   }
 
-  return `export default {
-  ${codes.join(`,\n`)}
-}`
+  return `const isObject = (item) => item && typeof item === 'object' && !Array.isArray(item);
+
+const mergeDeep = (target, ...sources) => {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
+
+export default mergeDeep({},
+  ${codes.map(code => `{${code}}`).join(',\n')}
+);`
 }
 
 async function getCode(

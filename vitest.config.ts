f150059159baf4227b96fcbc39702a3f5167b389
kazuya kawaguchi <kawakazu80@gmail.com>
2024-05-17 17:21:13 +0900

    chore: tweak deps for intlify modules

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@intlify/message-compiler':
+        '@intlify/message-compiler/dist/message-compiler.node.mjs'
+    }
+  },
   test: {
     globals: true
   }

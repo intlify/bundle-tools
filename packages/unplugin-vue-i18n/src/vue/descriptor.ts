cbe60c7a8e803aa2e2444a8d9ddb513e58b2b65c
kazuya kawaguchi <kawakazu80@gmail.com>
2024-06-18 13:37:24 +0900

    fix: remove unnecesary hmr codes

--- packages/unplugin-vue-i18n/src/vue/descriptor.ts
+++ packages/unplugin-vue-i18n/src/vue/descriptor.ts
@@ -3,9 +3,6 @@
 // Auther: Evan You (https://github.com/yyx990803)
 // Forked from: https://github.com/vitejs/vite/tree/main/packages/plugin-vue
 
-import path from 'node:path'
-import { normalizePath } from '../utils'
-
 import type { CompilerError, SFCDescriptor } from 'vue/compiler-sfc'
 import type { VuePluginResolvedOptions } from './utils'
 
@@ -18,22 +15,13 @@ export interface SFCParseResult {
 export function createDescriptor(
   filename: string,
   source: string,
-  { root, template, compiler }: VuePluginResolvedOptions
-  // { root, isProduction, sourceMap, compiler, template }: ResolvedOptions,
-  // hmr = false
+  { template, compiler }: VuePluginResolvedOptions
 ): SFCParseResult {
   const { descriptor, errors } = compiler.parse(source, {
     filename,
     templateParseOptions: template?.compilerOptions
-    // sourceMap,
-    // templateParseOptions: template?.compilerOptions
   })
 
-  // ensure the path is normalized in a way that is consistent inside
-  // project (relative to root) and on different systems.
-  // const normalizedPath = normalizePath(path.relative(root!, filename))
-  // descriptor.id = getHash(normalizedPath + (isProduction ? source : ''))
-  // ;(hmr ? hmrCache : cache).set(filename, descriptor)
   return { descriptor, errors }
 }
 
@@ -41,10 +29,6 @@ export function getDescriptor(
   filename: string,
   code: string,
   options: VuePluginResolvedOptions
-  // options: ResolvedOptions,
-  // createIfNotFound = true,
-  // hmr = false,
-  // code?: string
 ): SFCDescriptor {
   const { descriptor, errors } = createDescriptor(filename, code, options)
   if (errors.length) {
@@ -52,20 +36,4 @@ export function getDescriptor(
   }
 
   return descriptor
-  // const _cache = hmr ? hmrCache : cache
-  // if (_cache.has(filename)) {
-  //   return _cache.get(filename)!
-  // }
-  // if (createIfNotFound) {
-  //   const { descriptor, errors } = createDescriptor(
-  //     filename,
-  //     code ?? fs.readFileSync(filename, 'utf-8'),
-  //     options,
-  //     hmr
-  //   )
-  //   if (errors.length && !hmr) {
-  //     throw errors[0]
-  //   }
-  //   return descriptor
-  // }
 }

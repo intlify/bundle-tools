1277d70f6be00998c96cbe6fc163c3005a973f7e
userquin <userquin@gmail.com>
2024-11-16 15:15:33 +0100

    feat!: move packages to ESM first

--- packages/unplugin-vue-i18n/build.config.ts
+++ packages/unplugin-vue-i18n/build.config.ts
@@ -1,4 +1,9 @@
+import { readFile, rm, writeFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
+
+const lib = fileURLToPath(new URL('./lib', import.meta.url))
 
 export default defineBuildConfig({
   declaration: true,
@@ -24,5 +29,27 @@ export default defineBuildConfig({
   rollup: {
     emitCJS: true
   },
-  externals: ['vite', 'webpack']
+  externals: ['vite', 'webpack'],
+  hooks: {
+    'build:done': async () => {
+      await Promise.all([
+        rm(resolve(lib, 'types.cjs')),
+        rm(resolve(lib, 'types.mjs')),
+        ...['vite', 'webpack'].map(async name => {
+          for (const ext of ['d.ts', 'd.cts']) {
+            const path = resolve(lib, `${name}.${ext}`)
+            const content = await readFile(path, 'utf-8')
+            await writeFile(
+              path,
+              content.replace(
+                `export { ${name} as default };`,
+                `export = ${name};`
+              ),
+              { encoding: 'utf-8' }
+            )
+          }
+        })
+      ])
+    }
+  }
 })

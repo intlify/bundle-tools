9298f987a77b5ae999079de61635f0e63a4e7d69
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-08 11:32:42 +0100

    test: split snapshots only for `unplugin-vue-i18n` tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,22 @@
 import { basename, dirname, join } from 'node:path'
 import { defineConfig } from 'vitest/config'
 
+const resolveSnapshotPath = (testPath, extension) => {
+  // only split snapshots for unplugin-vue-i18n tests
+  if (testPath.includes('unplugin-vue-i18n/test')) {
+    const framework = '.' + process.env.TEST_FRAMEWORK || 'vite'
+    return join(
+      join(dirname(testPath), '__snapshots__'),
+      `${basename(testPath)}${framework}${extension}`
+    )
+  }
+
+  return join(
+    join(dirname(testPath), '__snapshots__'),
+    `${basename(testPath)}${extension}`
+  )
+}
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -10,12 +26,6 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    resolveSnapshotPath(testPath, extension) {
-      const framework = '.' + process.env.TEST_FRAMEWORK || 'vite'
-      return join(
-        join(dirname(testPath), '__snapshots__'),
-        `${basename(testPath)}${framework}${extension}`
-      )
-    }
+    resolveSnapshotPath
   }
 })

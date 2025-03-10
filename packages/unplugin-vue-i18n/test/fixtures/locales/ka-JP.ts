4909a892d732b7b29f1eb79845eb03e29b1ed78a
daguanren21 <1363917353@qq.com>
2025-03-10 15:00:38 +0800

    fix： ci error

--- packages/unplugin-vue-i18n/test/fixtures/locales/ka-JP.ts
+++ packages/unplugin-vue-i18n/test/fixtures/locales/ka-JP.ts
@@ -1,11 +1,11 @@
-import ja from './ja.json'
+// import ja from './ja.json'
 
-export default function loadResource(url) {
-  return new Promise(resolve => {
-    fetch(url)
-      .then(response => response.json())
-      .then(res => {
-        resolve({ ...ja, ...res })
-      })
-  })
-}
+// export default function loadResource(url) {
+//   return new Promise(resolve => {
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(res => {
+//         resolve({ ...ja, ...res })
+//       })
+//   })
+// }

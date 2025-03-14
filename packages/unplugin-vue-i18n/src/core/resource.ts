9421343e5779e93cf7340ca26ca3e43d705aaa63
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-15 00:31:31 +0100

    fix: include transform resource paths

--- packages/unplugin-vue-i18n/src/core/resource.ts
+++ packages/unplugin-vue-i18n/src/core/resource.ts
@@ -263,6 +263,7 @@ export function resourcePlugin(
 
       const { filename } = parseVueRequest(id)
       return (
+        resourcePaths.has(id) ||
         filename.endsWith('vue') ||
         filename.endsWith(INTLIFY_BUNDLE_IMPORT_ID) ||
         (/\.(json5?|ya?ml)$/.test(filename) && filter(filename))

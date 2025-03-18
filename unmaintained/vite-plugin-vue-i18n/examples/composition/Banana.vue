77e1be436e661527a790ce83291280fb46557f25
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-18 18:17:59 +0900

    chore: add more files

--- unmaintained/vite-plugin-vue-i18n/examples/composition/Banana.vue
+++ unmaintained/vite-plugin-vue-i18n/examples/composition/Banana.vue
@@ -1,6 +1,6 @@
 <template>
   <form id="fruits">
-    <label>{{ t('select') }}</label>
+    <label>{{ t("select") }}</label>
     <select v-model.number="select">
       <option value="0">0</option>
       <option value="1">1</option>
@@ -8,19 +8,19 @@
       <option value="3">3</option>
     </select>
   </form>
-  <p>{{ t('fruits.banana', select, { n: select }) }}</p>
+  <p>{{ t("fruits.banana", select, { n: select }) }}</p>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
 
 export default {
-  name: 'Banana',
+  name: "Banana",
   setup() {
-    const { t } = useI18n({ useScope: 'global' })
-    const select = ref(0)
-    return { t, select }
-  }
-}
+    const { t } = useI18n({ useScope: "global" });
+    const select = ref(0);
+    return { t, select };
+  },
+};
 </script>

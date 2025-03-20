64c4780eaef3639c615ce9b67650318118dab464
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-20 19:42:55 +0900

    fix: hide unmaintained packages

--- eslint.config.ts
+++ eslint.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(
     name: 'ignores',
     ignores: [
       'examples/**',
-      'unmaintained/**',
+      '.unmaintained/**',
       '**/test/fixtures/**',
       '**/*.config.ts',
       '**/dist/**',
--- knip.config.ts
+++ knip.config.ts
@@ -5,7 +5,7 @@ export default {
     'scripts/playwright.ts', // jiti
     'examples/rspack/rspack.config.mjs' // cannot resolve rspack plugin ...
   ],
-  ignore: ['**/fixtures/**', 'unmaintained/**'],
+  ignore: ['**/fixtures/**', '.unmaintained/**'],
   ignoreDependencies: ['@babel/preset-typescript', 'ts-loader', 'lint-staged'],
   ignoreBinaries: ['dev']
 } satisfies KnipConfig

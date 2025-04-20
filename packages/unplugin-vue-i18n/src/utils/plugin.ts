03e03a76727393867af7d0c2104d6dda9eeb7036
kazuya kawaguchi <kawakazu80@gmail.com>
2025-04-20 12:27:56 +0900

    fix(unpluing-vue-i18n): Support for `transform.handler` in vite:json plugin (#484)

--- packages/unplugin-vue-i18n/src/utils/plugin.ts
+++ packages/unplugin-vue-i18n/src/utils/plugin.ts
@@ -15,6 +15,34 @@ export function getVitePlugin(config: UserConfig, name: string): RollupPlugin | 
   return config.plugins.find(p => p.name === name) as RollupPlugin
 }
 
+export function getVitePluginTransform(
+  plugin: RollupPlugin
+): [RollupPlugin['transform'], 'handler' | 'transform' | undefined] {
+  if (plugin.transform) {
+    return 'handler' in plugin.transform
+      ? [plugin.transform.handler, 'handler']
+      : [plugin.transform, 'transform']
+  } else {
+    return [undefined, undefined]
+  }
+}
+
+// TODO: `override` type, we need more strict type
+export function overrideVitePluginTransform(
+  plugin: RollupPlugin,
+  override: Function,
+  to: 'handler' | 'transform'
+): void {
+  if (plugin.transform == undefined) {
+    throw new Error('plugin.transform is undefined')
+  }
+  if (to === 'handler' && 'handler' in plugin.transform) {
+    plugin.transform.handler = override as typeof plugin.transform.handler
+  } else {
+    plugin.transform = override as typeof plugin.transform
+  }
+}
+
 export function checkVuePlugin(vuePlugin: RollupPlugin | null): boolean {
   if (vuePlugin == null || !vuePlugin.api) {
     error(

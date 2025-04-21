730c0138196cffe55372758d600a0a03ae4eba5f
kazuya kawaguchi <kawakazu80@gmail.com>
2025-04-21 10:59:14 +0900

    Revert "fix(unplugin-vue-i18n): Support for transform.handler in vite:json pl…"
    This reverts commit b8900e3e2fd0b334ffdabf372239c722e382a96f.

--- packages/unplugin-vue-i18n/src/core/resource.ts
+++ packages/unplugin-vue-i18n/src/core/resource.ts
@@ -21,8 +21,6 @@ import {
   checkVuePlugin,
   error,
   getVitePlugin,
-  getVitePluginTransform,
-  overrideVitePluginTransform,
   raiseError,
   resolveNamespace,
   warn
@@ -138,24 +136,11 @@ export function resourcePlugin(
           `configResolved: isProduction = ${isProduction}, sourceMap = ${sourceMap}`
         )
 
-        /**
-         * NOTE:
-         * For the native rolldown plugin, we need to change to another solution from the current workaround.
-         * Currently, the rolldown team and vite team are discussing this issue.
-         * https://github.com/vitejs/rolldown-vite/issues/120
-         */
-
         // json transform handling
         const jsonPlugin = getVitePlugin(config, 'vite:json')
         if (jsonPlugin) {
-          // saving `vite:json` plugin instance
-          const [orgTransform, transformWay] =
-            getVitePluginTransform(jsonPlugin)
-          if (!orgTransform) {
-            throw new Error('vite:json plugin not found!')
-          }
-
-          async function overrideViteJsonPlugin(code: string, id: string) {
+          const orgTransform = jsonPlugin.transform // backup @rollup/plugin-json
+          jsonPlugin.transform = async function (code: string, id: string) {
             if (!/\.json$/.test(id) || filter(id)) {
               return
             }
@@ -177,13 +162,6 @@ export function resourcePlugin(
             // @ts-expect-error
             return orgTransform!.apply(this, [code, id])
           }
-
-          // override `vite:json` plugin transform function
-          overrideVitePluginTransform(
-            jsonPlugin,
-            overrideViteJsonPlugin,
-            transformWay!
-          )
         }
 
         /**

3fedbe09a57267a4b60210797022434f886d8694
Bobbie Goede <bobbiegoede@gmail.com>
2025-05-10 06:27:26 +0200

    refactor: extract default options to constant object (#493)
    * refactor: extract default options to constant object
    
    * chore: spelling

--- packages/bundle-utils/src/ts.ts
+++ packages/bundle-utils/src/ts.ts
@@ -7,58 +7,26 @@
 import { transform } from 'esbuild'
 import { Node } from 'estree'
 import { CodeGenOptions, CodeGenResult } from './codegen'
-import { generate as generateJavaScript } from './js'
+import { DEFAULT_OPTIONS, generate as generateJavaScript } from './js'
 
 /**
  * @internal
  */
 export async function generate(
   targetSource: string | Buffer,
-  {
-    type = 'plain',
-    filename = 'vue-i18n-loader.js',
-    inSourceMap = undefined,
-    locale = '',
-    isGlobal = false,
-    sourceMap = false,
-    env = 'development',
-    forceStringify = false,
-    onError = undefined,
-    onWarn = undefined,
-    strictMessage = true,
-    escapeHtml = false,
-    allowDynamic = false,
-    jit = false
-  }: CodeGenOptions
+  options: CodeGenOptions
 ): Promise<CodeGenResult<Node>> {
   let value = Buffer.isBuffer(targetSource) ? targetSource.toString() : targetSource
 
-  const options = {
-    type,
-    source: value,
-    sourceMap,
-    locale,
-    isGlobal,
-    inSourceMap,
-    env,
-    filename,
-    forceStringify,
-    onError,
-    onWarn,
-    strictMessage,
-    escapeHtml,
-    allowDynamic,
-    jit
-  } as CodeGenOptions
-
-  if (options.filename && /.[c|m]?ts$/.test(options.filename)) {
+  const _options = Object.assign({}, DEFAULT_OPTIONS, options, { source: value })
+  if (_options.filename && /.[c|m]?ts$/.test(_options.filename)) {
     const transformed = await transform(value, { loader: 'ts' })
 
     if (transformed && transformed.code) {
       value = transformed.code
-      options.source = transformed.code
+      _options.source = transformed.code
     }
   }
 
-  return generateJavaScript(value, options)
+  return generateJavaScript(value, _options)
 }
